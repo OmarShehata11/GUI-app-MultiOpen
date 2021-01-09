4cedac8a5c077b5661dea332ae8ebe4e27b65932
Omar Shehata <71821822+OmarShehata11@users.noreply.github.com>
2021-01-09 08:08:33 +0200

    fitting the window screen , and aslo adding the incon (but it's not working actually ) and at last adding the quit button to the program

--- the app.py
+++ the app.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from tkinter import filedialog, Text
+from tkinter import filedialog
 import os
-
 
 if __name__ == '__main__':
     main = Tk()
-    main.geometry("900x700")
+    main.geometry("350x350")
     main.title("first GUI program i have made ")
+    main.iconbitmap('D:/the channel/LogoMakr-252PUy.png')
 
 
 def checkPass():
@@ -18,9 +18,9 @@ def checkPass():
         label.pack()
 
 
-pass_label = Label(main, text="enter the password to open the program ")
-password_box = Entry(main, width=20)
-submit_button = Button(main, text="confirm", command=checkPass)
+pass_label = Label(main, text="enter the password to open the program ", fg="red")
+password_box = Entry(main, width=20, borderwidth=5)
+submit_button = Button(main, text="confirm", command=checkPass, bg="black", fg="red")
 
 pass_label.pack()
 password_box.pack()
@@ -28,11 +28,11 @@ submit_button.pack()
 
 
 def play_apps():
-    main.geometry("900x900")
+    main.geometry("900x700")
     for widget in main.winfo_children():
         widget.destroy()
 
-    body = Canvas(main, height=700, width=900, bg='lightblue')
+    body = Canvas(main, height=580, width=900, bg='purple')
     body.pack()
     main.title("GUI first app")
     apps = []  # empty list
@@ -40,7 +40,7 @@ def play_apps():
     if os.path.isfile("save.txt"):
         with open('save.txt', "r") as g:
             tempFiles = g.read()
-            tempFiles = tempFiles.split(',')
+            tempFiles = tempFiles.split('\n')
             apps = [x for x in tempFiles if x.strip()]
 
     def deleteApps():
@@ -55,26 +55,30 @@ def play_apps():
         apps.append(fileName)
         print(fileName)
         for app in apps:
-            label = Label(frame, text=app, bg="red")
+            label = Label(frame, text=app, bg="white")
             label.pack()
 
     def runFiles():
         for app in apps:
             os.startfile(app)
 
-    frame = Frame(body, bg="lightgreen")
+    frame = Frame(body, bg="black")
 
     frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-    openFile = Button(main, text="open file", padx=10, pady=5, fg="white", bg="black", command=addFiles)
-    runApps = Button(main, text="run the apps", padx=10, pady=7, fg="white", bg="black", command=runFiles)
+    quit_button = Button(main, text="Quit the program", pady=5, padx=0, borderwidth=5, command=main.quit)
+    openFile = Button(main, text="open file", padx=10, pady=5, fg="purple", bg="black", command=addFiles)
+    runApps = Button(main, text="run the apps", padx=10, pady=7, fg="purple", bg="black", command=runFiles)
 
     openFile.pack()
     runApps.pack()
+    quit_button.pack()
 
     for app in apps:
         label = Label(frame, text=app)
         label.pack()
+
+    main.mainloop()
 
     with open('save.txt', 'w') as f:
         for app in apps:
@@ -83,5 +87,10 @@ def play_apps():
 
 main.mainloop()
 
-# add new button for deleting the apps the user want
-# by also using a delete button contain the letter "x" in front of all the apps
+"""
+
+some notes i want to add to the program::
+1) add new button for deleting the apps the user want
+- by also using a delete button contain the letter "x" in front of all the apps
+
+"""
